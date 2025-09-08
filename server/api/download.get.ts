4e610a06b079228d0fca9abd3cd5a9f5cee7c970
ketsukan-takahashi <ketsukan-takahashi@uchida.co.jp>
2025-09-08 13:12:01 +0900

    e

--- server/api/download.get.ts
+++ server/api/download.get.ts
@@ -6,8 +6,10 @@ export default defineEventHandler(async (event) => {
         return { error: 'url is required' };
     }
     console.log('url', url);
+    const urls = `https://ucdgovtest.g.kuroco.app/direct/topics/topics_file_download/?topics_id=14&ext_no=2&index=0`;
+
     // Kuroco にリクエスト
-    const res = await fetch(url.toString(), {
+    const res = await fetch(urls, {
         headers: {
             Authorization: `Bearer ${process.env.KUROCO_API_TOKEN}` // 管理者トークンを環境変数に設定
         }
